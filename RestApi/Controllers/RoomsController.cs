595b8cb9e1b2cffd8edd4dddd1ed294f824ca203
David Solomon <dav.soli22@gmail.com>
2020-05-03 19:12:33 +0200

    Resolve conflict

--- RestApi/Controllers/RoomsController.cs
+++ RestApi/Controllers/RoomsController.cs
@@ -10,49 +10,51 @@ using Entities.RequestFeatures;
 
 namespace RestApi.Controllers
 {
-
-  [Route("api/rooms")]
-  [ApiController]
-  public class RoomsController : ControllerBase
-  {
-    private readonly IRepositoryManager _repository;
-    private readonly IMapper _mapper;
-
-    public RoomsController(IRepositoryManager repository, IMapper mapper)
+    [Route("api/rooms")]
+    [ApiController]
+    public class RoomsController : ControllerBase
     {
-      _repository = repository;
-      _mapper = mapper;
-    }
+        private readonly IRepositoryManager _repository;
+        private readonly IMapper _mapper;
 
-    [HttpPost]
-    public async Task<IActionResult> CreateRoom([FromBody]RoomForCreationDto room)
-    {
-      if (room == null)
-      {
-        return BadRequest("RoomForCreationDto object is null");
-      }
+        public RoomsController(IRepositoryManager repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
 
-      if (!ModelState.IsValid)
-      {
-        return UnprocessableEntity(ModelState);
-      }
+        [HttpPost]
+        public async Task<IActionResult> CreateRoom([FromBody]RoomForCreationDto room)
+        {
+            if (room == null)
+            {
+                return BadRequest("RoomForCreationDto object is null");
+            }
 
-      var roomEntity = _mapper.Map<Room>(room);
+            if (!ModelState.IsValid)
+            {
+                return UnprocessableEntity(ModelState);
+            }
 
-      _repository.Room.CreateRoom(roomEntity);
-      await _repository.SaveAsync();
+            var roomEntity = _mapper.Map<Room>(room);
 
-      var roomToReturn = _mapper.Map<RoomDto>(roomEntity);
+            _repository.Room.CreateRoom(roomEntity);
+            await _repository.SaveAsync();
 
-      return CreatedAtRoute("RoomById", new { id = roomToReturn.Id }, roomToReturn);
-    }
+            var roomToReturn = _mapper.Map<RoomDto>(roomEntity);
 
-    [HttpGet]
-    public async Task<IActionResult> GetRooms([FromQuery] RoomParameters roomParameters)
-    {
-      var rooms = await _repository.Room.GetAllRoomsAsync(roomParameters, trackChanges: false);
+            return CreatedAtRoute("RoomById", new { id = roomToReturn.Id }, roomToReturn);
+        }
 
-      var roomsDto = _mapper.Map<IEnumerable<RoomDto>>(rooms);
+        [HttpGet]
+        public async Task<IActionResult> GetRooms([FromQuery] RoomParameters roomParameters)
+        {
+            var rooms = await _repository.Room.GetAllRoomsAsync(roomParameters, trackChanges: false);
+
+            var roomsDto = _mapper.Map<IEnumerable<RoomDto>>(rooms);
+
+            return Ok(roomsDto);
+        }
 
         [HttpGet("available")]
         public async Task<IActionResult> GetAvailableRooms([FromQuery] RoomParameters roomParameters)
@@ -74,64 +76,62 @@ namespace RestApi.Controllers
 
             var roomsDto = _mapper.Map<IEnumerable<RoomDto>>(rooms);
 
-      var roomsDto = _mapper.Map<IEnumerable<RoomDto>>(rooms);
+            return Ok(roomsDto);
+        }
 
-      return Ok(roomsDto);
+        [HttpGet("{id}", Name = "RoomById")]
+        public async Task<IActionResult> GetRoom(int id)
+        {
+            var room = await _repository.Room.GetRoomAsync(id, trackChanges: false);
+            if (room == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var roomDto = _mapper.Map<RoomDto>(room);
+                return Ok(roomDto);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateRoom(int id, [FromBody] RoomForUpdateDto room)
+        {
+            if(room == null)
+            {
+                return BadRequest("RoomForUpdateDto object is null");
+            }
+
+            if(!ModelState.IsValid)
+            {
+                return UnprocessableEntity(ModelState);
+            }
+
+            var roomEntity = await _repository.Room.GetRoomAsync(id, trackChanges: true);
+            if(roomEntity == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(room, roomEntity);
+            await _repository.SaveAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRoom(int id)
+        {
+            var room = await _repository.Room.GetRoomAsync(id, trackChanges: false);
+            if(room == null)
+            {
+                return NotFound();
+            }
+
+            _repository.Room.DeleteRoom(room);
+            await _repository.SaveAsync();
+
+            return NoContent();
+        }
     }
-
-    [HttpGet("{id}", Name = "RoomById")]
-    public async Task<IActionResult> GetRoom(int id)
-    {
-      var room = await _repository.Room.GetRoomAsync(id, trackChanges: false);
-      if (room == null)
-      {
-        return NotFound();
-      }
-      else  
-      {
-        var roomDto = _mapper.Map<RoomDto>(room);
-        return Ok(roomDto);
-      }
-    }
-
-    [HttpPut("{id}")]
-    public async Task<IActionResult> UpdateRoom(int id, [FromBody] RoomForUpdateDto room)
-    {
-      if (room == null)
-      {
-        return BadRequest("RoomForUpdateDto object is null");
-      }
-
-      if (!ModelState.IsValid)
-      {
-        return UnprocessableEntity(ModelState);
-      }
-
-      var roomEntity = await _repository.Room.GetRoomAsync(id, trackChanges: true);
-      if (roomEntity == null)
-      {
-        return NotFound();
-      }
-
-      _mapper.Map(room, roomEntity);
-      await _repository.SaveAsync();
-
-      return NoContent();
-    }
-
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteRoom(int id)
-    {
-      var room = await _repository.Room.GetRoomAsync(id, trackChanges: false);
-      if (room == null)
-      {
-        return NotFound();
-      }
-
-      _repository.Room.DeleteRoom(room);
-      await _repository.SaveAsync();
-
-      return NoContent();
-    }
-  }
 }
